cb0bf1077ccf47da1971e76f3c8d3e99ad2f5edb
cytvictor <cytvictor@yeah.net>
2017-08-28 12:41:18 +0800

    changed configs

--- Application/Common/Conf/route.php
+++ Application/Common/Conf/route.php
@@ -4,6 +4,8 @@ return array(
     'URL_ROUTER_ON'   => true,
 
     'URL_MAP_RULES' => array(
+        'login' => 'Home/Auth/login',
+        'register' => 'Home/Auth/register',
         
     ),
 );
